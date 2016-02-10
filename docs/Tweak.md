5358c08b70e94a7f166be5f4ae72f5a0308e05bd
harre096 <harre096@umn.edu>
2016-02-09 21:15:10 -0600

    cleaning up our tweak notes and code

--- docs/Tweak.md
+++ docs/Tweak.md
@@ -1,5 +1,5 @@
 ### Tweak Idea 1
 ##### Flip a random bit. (flip-one-bit)
-First we run the random generator n times, then use our hill climbing operator m times.
-* We think that randomly exchanging one item for another may not be super effective, but should be a reasonable first mutator.
-* Also, if we run random enough times up front, we should have a reasonalble hill to climb.
+First we run the random generator n times to give us a decent base case, then use our hill climbing operator m times.
+* We think that randomly exchanging one item for another may not be the most effective, but should be a reasonable first mutator.
+* Our answer will climb that hill by swapping in or out one random item at a time, and will only keep that change if it was an improvement.
--- src/simple_search/core.clj
+++ src/simple_search/core.clj
@@ -31,6 +31,7 @@
      :total-weight (reduce + (map :weight included))
      :total-value (reduce + (map :value included))}))
 
+
 ;;; It might be cool to write a function that
 ;;; generates weighted proportions of 0's and 1's.
 
@@ -106,24 +107,8 @@
   )
 )
 
-;; answer -> (mutated) answer
-(defn swap-random-item
-  "Given an instance, we intend to flip a random bit off and a random bit on."
-  [answer]
-  (let [size (count (:choices answer)),
-        flip1 (rand-int size),
-        flip2 (rand-int size),
-        flip3 (rand-int size),
-        choices (vec (:choices answer))]
-    (assoc answer :choices (assoc choices flip1 (findFlipVal choices flip1)))
-  )
-)
-
 
 ;; (find-score (flip-one-bit (random-search knapPI_16_20_1000_1 1))
-;; )
-
-;; (find-score (swap-random-item (random-search knapPI_16_20_1000_1 1))
 ;; )
 
 
